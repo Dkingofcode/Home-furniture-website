2a5965146d1143e02cf5ed5bbbc666eb5787e37e
Dkingofcode <doladepo128@gmail.com>
2023-04-10 00:45:58 +0100

    First commit

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+     <h2>Loading.....</h2>
+      <h4>loading element</h4>
+    </div>
+  )
+}
+
+export default Loading
